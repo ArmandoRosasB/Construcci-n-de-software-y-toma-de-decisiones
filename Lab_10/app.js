b6e427be46e7270acf590ef24d944db875f9797d
ArmandoRosasB <armando.rosas133@gmail.com>
2023-03-04 17:29:42 -0600

    Lab10: Completed

---
+++ Lab_10/app.js
@@ -0,0 +1,155 @@
+const filesystem = require('fs');
+const readline = require('readline-sync');
+const http = require('http');
+const httpStatus = require('http-status-codes');
+
+function fileExists(path) {
+    try {
+        let stats = filesystem.statSync(path);
+        return true;
+      }
+      catch(err) {
+        if (err.code == 'ENOEF'){
+            return false;
+        }
+      }
+}
+
+
+function toTextFile(title, string){
+    let name;
+
+    if (title.match(/^([A-Za-z0-9_])+\.txt$/)){
+        name = title;
+    }
+    else {
+        name = `${title}.txt`;
+    }
+
+    if (!fileExists(name)){
+        filesystem.writeFileSync(name, string);
+    } else {
+        filesystem.appendFileSync(name, `\n${string}`);
+    }
+
+}
+
+
+function getViewUrl(url){ //Views Directory: html pages
+    return `views${url}`;
+};
+
+
+function sendErrorResponse(res) { // Not found
+    res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
+};
+
+function customReadFile(file_path, res){
+    if (filesystem.existsSync(file_path)) {
+        filesystem.readFile(file_path, (error, data) => {
+            if (error) {
+                console.error(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else {
+        sendErrorResponse(res);
+    }
+};
+
+
+console.log("Inizializando Servidor");
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    let url = request.url;
+    if (url === '/'){
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(getViewUrl('/Index.html'), response);
+    }
+    else if (url.indexOf(".") == -1) {
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(getViewUrl(`${url}.html`), response);
+
+    } else if (url.indexOf(".html") !== -1) {
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(getViewUrl(`${url}`), response);
+    } else if (url.indexOf(".js") !== -1) {
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/javascript"
+        });
+        customReadFile(`./public${url}`, response);
+    } else if (url.indexOf(".css") !== -1) {
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/css"
+        });
+        customReadFile(`./public${url}`, response);
+    } else if (url.indexOf(".png") !== - 1) {
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/png"
+        });
+        customReadFile(`./public${url}`, response);
+    } else if (url.indexOf(".svg") !== -1){
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/svg+xml"
+        });
+        customReadFile(`./public${url}`, response);
+    } else if (url.indexOf(".jpeg") !== -1){
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/jpeg"
+        });
+        customReadFile(`./public${url}`, response);
+    } else if (url.indexOf(".webp") !== -1){
+        response.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/webp"
+        });
+        customReadFile(`./public${url}`, response);
+    }
+    else {
+        sendErrorResponse(response);
+    }
+ 
+    if ( request.method == "POST"){
+        const datos = []
+        request.on('data', (dato) => {
+            datos.push(dato);
+        })
+
+        return request.on("end", () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            const user = datos_completos.split('&');
+            const post = [];
+
+            user.forEach(element => {
+                if (element.indexOf("%40") !== -1){
+                    element = `${element.slice(0, element.indexOf('%'))}@${element.slice(element.indexOf('%') + 3)}`;
+                }
+                post.push(element.slice(element.indexOf('=') + 1));
+            })
+            
+            let text = `Name: ${post[0]}\nLastName: ${post[1]}\n$Email: ${post[2]}\nPreferred programming language: ${post[3]}\nMessage: ${post[4]}\n\n`;
+
+            toTextFile(post[0] + post[1], text.replaceAll("+", " "));
+            
+        })
+
+
+    }
+    
+})
+
+server.listen(port);
+console.log(`The server is listening on port: ${port}`);
---
+++ Lab_10/public/Scripts/app_4.js
@@ -0,0 +1,90 @@
+function isSafeEnough(password){
+    let valid_number = 0;
+    if (password.match(/^([^A-Za-z0-9_]{1,3}[A-Za-z0-9_]{1,3})|([A-Za-z0-9_]{1,3}[^A-Za-z0-9_]{1,3}){2,}$/) != null){
+        console.log("pwd super validada")
+
+        valid_number++;
+    }
+    
+
+    return valid_number;
+}
+
+function moveBar(level) {
+    if (level == 1) {
+        if (clientWidth = document.getElementById('progress-bar').clientWidth < 1){
+            let elem = document.getElementById("progress-bar");
+            let width = 1;
+            let id = setInterval(frame, 10);
+            function frame() {
+                if (width < 100) {
+                    width++;
+                    elem.style.width = width + "%";
+                }
+            }
+        }
+    }
+    else {
+        if (clientWidth = document.getElementById('progress-bar').clientWidth >299){
+            let elem = document.getElementById("progress-bar");
+            let width = 100;
+            let id = setInterval(frame, 10);
+            function frame() {
+                if (width > 0) {
+                    width--;
+                    elem.style.width = width + "%";
+                }
+            }
+        }
+    }
+}
+
+
+let pwd = document.getElementById("pwd");
+let cpwd = document.getElementById("c-pwd");
+
+pwd.onkeyup = () => {
+    if (isSafeEnough(pwd.value) === 1) {
+        moveBar(1);
+        pwd.style.background = "rgb(23, 228, 239)";
+    }
+    else{
+        moveBar(0);
+        pwd.style.background = "#ff6347";      
+    }
+}
+
+
+cpwd.onkeyup = () => {
+
+    if (cpwd.value === document.getElementById("pwd").value && isSafeEnough(cpwd.value)){
+        document.getElementById("Go").innerHTML =
+        "<input type=\"button\" onclick=\"location.href='page.html'\" value=\"Go\"/>"; 
+
+        const colors = document.querySelectorAll(".not-color");
+
+        colors.forEach( (container) => {
+            container.classList.replace("not-color", "p-color");
+        })
+
+        cpwd.style.background = "rgb(23, 228, 239)";
+    }  
+    else{
+        document.getElementById("Go").innerHTML =""; 
+        const colors = document.querySelectorAll(".p-color");
+
+        colors.forEach( (container) => {
+            container.classList.replace("p-color", "not-color");
+        })
+
+        cpwd.style.background = "#ff6347";
+    }
+}
+
+let requisitos = document.getElementById("advice");
+
+requisitos.onmouseover = () => {
+    alert("Requirements password");
+    alert("Have at least 1 sequence of alphanumeric characters and 1 sequence of non-alphanumeric characters. This at least 2 times");
+    alert("Warning: Sequences must be between 1 and 3 characters");
+}
