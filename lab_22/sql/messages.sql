c99d3a601f9432378453cb523905d3104a36dce3
ArmandoRosasB <armando.rosas133@gmail.com>
2023-03-27 17:29:07 -0600

    Lab22: More friends

--- lab_22/sql/messages.sql
+++ lab_22/sql/messages.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2023 at 05:30 AM
+-- Generation Time: Mar 28, 2023 at 01:24 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,10 +20,10 @@ SET time_zone = "+00:00";
 --
 -- Database: `messages`
 --
+
+-- --------------------------------------------------------
 CREATE DATABASE messages;
 USE messages;
--- --------------------------------------------------------
-
 --
 -- Table structure for table `mensaje`
 --
@@ -34,8 +34,20 @@ CREATE TABLE `mensaje` (
   `lname` varchar(30) DEFAULT NULL,
   `email` varchar(60) DEFAULT NULL,
   `Planguage` varchar(15) DEFAULT NULL,
-  `message` text DEFAULT NULL
+  `message` text DEFAULT NULL,
+  `img` varchar(400) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mensaje`
+--
+
+INSERT INTO `mensaje` (`id`, `fname`, `lname`, `email`, `Planguage`, `message`, `img`) VALUES
+(4, 'Ramona', 'Najera', 'A01423596@tec.mx', 'C', 'Que vivan los sapoooos!!!', '483-Ramona.jpeg'),
+(5, 'Rodrigo', 'Teran', 'rodrigo.teran@gmail.com', 'JavaScript', 'No se dice SQL, se dice Shkeleee!', '380-Teran.jpeg'),
+(6, 'Armando', 'Rosas', 'A01704132@tec.mx', 'Java', 'Que bonita pagina.', '812-Armando.jpeg'),
+(8, 'Uri', 'Gopar', 'A01709413@tec.mx', 'Python', 'Uwu.', '521-Uri.jpeg'),
+(9, 'Mafer', 'Moreno', 'A01708653@tec.mx', 'JavaScript', 'Stream Harry Styles!!!!', '108-Mafer.jpeg');
 
 -- --------------------------------------------------------
 
@@ -121,7 +133,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `username`, `password`, `created_at`) VALUES
-(1, 'Armando', 'ArmandoRosasB', '$2a$12$x8zzu.Jrorif8NvQtqQGFuN4N73xeNOEWkcwpluOCftSVCA.1jfVu', '2023-03-14 16:12:29');
+(1, 'Armando', 'ArmandoRosasB', '$2a$12$x8zzu.Jrorif8NvQtqQGFuN4N73xeNOEWkcwpluOCftSVCA.1jfVu', '2023-03-14 16:12:29'),
+(2, 'Armando Rosas', 'ArmandoRB', '$2a$12$.phogd6mCB62fk0OGU6UcePPVnVxkHdf6PFQIbSeVsnHBfQ9n5Ine', '2023-03-27 16:31:22');
 
 -- --------------------------------------------------------
 
@@ -140,7 +153,8 @@ CREATE TABLE `usuario_rol` (
 --
 
 INSERT INTO `usuario_rol` (`idUsuario`, `idRol`, `created_at`) VALUES
-(1, 1, '2023-03-15 04:30:26');
+(1, 1, '2023-03-15 04:30:26'),
+(2, 1, '2023-03-27 16:40:58');
 
 --
 -- Indexes for dumped tables
@@ -192,7 +206,7 @@ ALTER TABLE `usuario_rol`
 -- AUTO_INCREMENT for table `mensaje`
 --
 ALTER TABLE `mensaje`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `privilegios`
@@ -210,7 +224,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
